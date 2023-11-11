0fdb254ebec1b1e82bd6e0caf72c5a57a75e433d
Prasanth-Rj <rjprasanth967@gmail.com>
2023-11-11 09:31:24 +0530

    Created a personal notes React app

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>About my project</h1>
+      <p>I have developed a supplement ordering Website by MERN Stack</p>
+    </div>
+  );
+}
+export default Note;
